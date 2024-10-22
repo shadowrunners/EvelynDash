0b79466b66c81ca30cb80a753c01c17143ab0556
scrappie <notscrappie@proton.me>
2024-10-22 23:16:37 +0300

    chore(config): removed common.tsx

--- apps/frontend/src/config/common.tsx
+++
@@ -1,6 +0,0 @@
-// This will be removed in the final version.
-export const config = {
-	name: 'Evelyn',
-	inviteUrl:
-		'https://discord.com/oauth2/authorize?client_id=1104526040656785418&scope=bot',
-};
