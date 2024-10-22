abd10f2f26e32418db1b9b2a507d3dcec593079d
scrappie <notscrappie@proton.me>
2024-10-22 23:21:17 +0300

    feat(app): added Geist Font

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,7 +1,10 @@
-import { NextAuthProvider, QueryProvider } from '@/components/providers';
+import { NextAuthProvider, QueryProvider } from '@Providers';
 import type { ReactNode } from 'react';
 import { Metadata } from 'next';
 import '@/styles/global.css';
+
+import { GeistSans } from 'geist/font/sans'
+import { GeistMono } from 'geist/font/mono'
 
 export const metadata: Metadata = {
 	title: 'Evelyn | shadowworks.',
@@ -14,8 +17,8 @@ export default function RootLayout({
     children: ReactNode,
   }) {
 	return (
-		<html lang='en'>
-			<body>
+		<html lang='en' className={`${GeistMono.variable} ${GeistSans.variable}`} suppressHydrationWarning>
+			<body className='antialiased'>
 				<NextAuthProvider>
 					<QueryProvider>
 						{children}
