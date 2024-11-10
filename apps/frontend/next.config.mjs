754ad57445873a2a940eaee46b710edbba7980bc
scrappie <notscrappie@proton.me>
2024-11-10 13:09:55 +0200

    refactor(next): next.config.js -> next.config.mjs

--- apps/frontend/next.config.mjs
+++ apps/frontend/next.config.mjs
@@ -1,17 +1,11 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withNextIntl = require('next-intl/plugin')(
-	// This is the default (also the `src` folder is supported out of the box)
-	'./src/utils/i18n.ts',
-);
+import createNextIntlPlugin from 'next-intl/plugin';
+
+const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-module.exports = withNextIntl({
+const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-	images: {
-		domains: ['i.imgur.com', 'cdn.discordapp.com', 'res.cloudinary.com'],
-		unoptimized: true,
-	},
 	// Disables type validity.
 	// Pretty much a fucking necessity now since it spits out errors up the ass that I just can't be bothered to fix.
 	typescript: {
@@ -21,4 +15,16 @@ module.exports = withNextIntl({
 		// Temporary change for preview builds, so they can be previewed on devices outside my dev environment. :)
 		ignoreDuringBuilds: true,
 	},
-});
+	images: {
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'res.cloudinary.com',
+				port: '',
+				pathname: '/shadowrunners/image/**'
+			}
+		]
+	}
+};
+
+export default withNextIntl(nextConfig);
