301aad0d73d28ad1669a4511a086fb4655225671
scrappie <notscrappie@proton.me>
2024-11-23 17:22:57 +0200

    refactor(frontend): a lotta changes
    - Added a grid dot background to the Features section.
    - Added a support banner at the bottom of the page.
    - Replaced "Poppins Sans" with "Geist Sans".

--- apps/frontend/src/app/[locale]/page.tsx
+++ apps/frontend/src/app/[locale]/page.tsx
@@ -1,14 +1,10 @@
-import {
-	Features,
-	Footer,
-	Hero,
-	Navbar,
-	SupportBanner,
-} from '../../components/homepage';
+import { FAQ } from '@/components/homepage/home-faq';
+import { DotBackground } from '@/components/ui/dotbackground';
+import { Features, Footer, Hero, Navbar, SupportBanner } from '@components/homepage';
 
 export default function Main() {
 	return (
-		<div className='bg-black w-full overflow-hidden font-poppins font-semibold'>
+		<div className='bg-black w-full overflow-hidden font-sans font-semibold'>
 			<div className='sm:px-16 px-6 flex justify-center items-center'>
 				<div className='xl:max-w-[1920px] w-full'>
 					<Navbar />
@@ -19,9 +15,16 @@ export default function Main() {
 					<Hero />
 				</div>
 			</div>
-			<div className='bg-black sm:px-16 px-6 flex justify-center items-start'>
+			<DotBackground>
+				<div className='sm:px-16 px-6 flex justify-center items-start'>
+					<div className='xl:max-w-[1280px] w-full'>
+						<Features />
+					</div>
+				</div>
+			</DotBackground>
+			<div className='sm:px-16 px-6 flex justify-center items-start'>
 				<div className='xl:max-w-[1280px] w-full'>
-					<Features /> <SupportBanner /> <Footer /> 
+					<SupportBanner /> <Footer /> 
 				</div>
 			</div>
 		</div>
