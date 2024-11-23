8adaddb1f8127511f66ca735484dbfc09de3a652
scrappie <notscrappie@proton.me>
2024-11-23 17:04:21 +0200

    refactor(backend): updated guild module to reflect changes

--- apps/backend/src/guilds/guild.module.ts
+++ apps/backend/src/guilds/guild.module.ts
@@ -1,23 +1,34 @@
-import { Module } from '@nestjs/common';
+import { ThrottlerGuard } from '@nestjs/throttler';
 import { MongooseModule } from '@nestjs/mongoose';
+import { APP_GUARD } from '@nestjs/core';
+import { Module } from '@nestjs/common';
 
 import { GuildController } from './controllers/guild.controller';
-import { GuildSchema } from './schemas/guild.schema';
-import { BotService } from '../services/bot.service';
 import { GuildsService } from './services/guild.service';
-import { APP_GUARD } from '@nestjs/core';
-import { ThrottlerGuard } from '@nestjs/throttler';
+import { GuildSchema } from './schemas/guild.schema';
+import { BotModule } from '../bot/bot.module';
+import { BotService } from '../bot/services/bot.service';
+import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/services/users.service';
+import { EncryptionService } from './services/encryption.service';
 
 @Module({
-	imports: [MongooseModule.forFeature([{ name: 'Guilds', schema: GuildSchema }])],
+	imports: [
+		MongooseModule.forFeature([{ name: 'Guilds', schema: GuildSchema }]),
+		BotModule,
+		UsersModule,
+	],
 	controllers: [GuildController],
 	providers: [
-		BotService,
 		GuildsService,
+		BotService,
+		UsersService,
+		EncryptionService,
 		{
 			provide: APP_GUARD,
 			useClass: ThrottlerGuard,
 		},
 	],
+	exports: [GuildsService]
 })
 export class GuildsModule {}
