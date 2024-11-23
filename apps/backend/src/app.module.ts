5811d3f802b762158ece6490ab280e97a082ba5a
scrappie <notscrappie@proton.me>
2024-11-23 16:59:50 +0200

    refactor(backend): split everything into their own modules

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -4,15 +4,24 @@ import { AuthMiddleware } from './middlewares/auth.middleware';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ThrottlerModule } from '@nestjs/throttler';
 import { GuildsModule } from './guilds/guild.module';
+import { DevController } from './dev/controllers/dev.controller';
+import { DevModule } from './dev/dev.module';
+import { UsersModule } from './users/users.module';
+import { BotModule } from './bot/bot.module';
 
 @Module({
 	imports: [
-		MongooseModule.forRoot(process.env.DATABASE_URL), GuildsModule,
+		MongooseModule.forRoot(process.env.DATABASE_URL),
 		ThrottlerModule.forRoot([{ ttl: 30000, limit: 10 }]),
+		GuildsModule,
+		DevModule,
+		UsersModule,
+		BotModule,
 	],
 })
 export class AppModule implements NestModule {
 	configure(consumer: MiddlewareConsumer) {
 		consumer.apply(AuthMiddleware).forRoutes(GuildController);
+		consumer.apply(AuthMiddleware).forRoutes(DevController);
 	}
 }
