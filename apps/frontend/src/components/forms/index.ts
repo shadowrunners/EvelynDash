9ab947925f2a553077cb4bad8c43062a02f04a92
scrappie <notscrappie@proton.me>
2024-11-24 15:51:39 +0200

    refactor(frontend): updated forms index.ts to reflect changes

--- apps/frontend/src/components/forms/index.ts
+++ apps/frontend/src/components/forms/index.ts
@@ -3,15 +3,13 @@ import { SmallColorPickerForm } from './ColorPicker';
 import { Embed } from './Embed';
 import { RoleSelectForm, MultiRoleSelectForm } from './RoleSelect';
 import { TextAreaForm } from './TextArea';
-import { NumericTextForm } from './NumericInput';
 
-export { 
-    ChannelSelectForm,
-    MultiChannelSelectForm,
-    SmallColorPickerForm,
-    Embed,
-    RoleSelectForm,
-    MultiRoleSelectForm,
-    TextAreaForm,
-    NumericTextForm
+export {
+	ChannelSelectForm,
+	MultiChannelSelectForm,
+	SmallColorPickerForm,
+	Embed,
+	RoleSelectForm,
+	MultiRoleSelectForm,
+	TextAreaForm,
 };
