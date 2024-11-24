a99bfae2eb5922c097ec5a048d6f16c5e175b7eb
scrappie <notscrappie@proton.me>
2024-11-24 16:49:20 +0200

    refactor(frontend): text area form rework

---
+++ apps/frontend/src/components/forms/textform.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage, Textarea } from '@UI';
+import type { FormParams } from '@Types';
+
+export function TextForm({
+	form,
+	formName,
+	formLabel,
+	formDescription,
+}: FormParams) {
+	return (
+		<Form {...form}>
+			<form className='bg-muted/40 width-[50%] relative border-r-3xl space-y-6 p-4 shadow rounded-xl'>
+				<FormField
+					control={form.control}
+					name={formName}
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>
+								{formLabel}
+							</FormLabel>
+							<FormControl>
+								<Textarea placeholder='Write a cool message here! ✨' className='resize-none' {...field} />
+							</FormControl>
+							<FormDescription>
+								{formDescription}
+							</FormDescription>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+			</form>
+		</Form>
+	);
+};
