b7c285de06e70d06d547a874f0657065b2bee20c
scrappie <notscrappie@proton.me>
2024-12-01 13:40:23 +0200

    chore(frontend): styling

--- apps/frontend/src/components/ui/dynamic-breadcrumb.tsx
+++ apps/frontend/src/components/ui/dynamic-breadcrumb.tsx
@@ -1,38 +1,38 @@
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "./breadcrumb";
-import { useGuild } from "../contexts/guildcontext";
-import { usePathname } from "@/i18n/routing";
-import { Fragment } from "react";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from './breadcrumb';
+import { useGuild } from '../contexts/guildcontext';
+import { usePathname } from '@/i18n/routing';
+import { Fragment } from 'react';
 
 export function DynamicBreadcrumb() {
-    const guild = useGuild();
+	const guild = useGuild();
 
-    const pathname = usePathname();
-    const breadcrumbs = [
-        { href: '/', label: 'Home' },
-        { href: `/guilds/${guild?.data.id}`, label: guild?.data.name },
-    ];
+	const pathname = usePathname();
+	const breadcrumbs = [
+		{ href: '/', label: 'Home' },
+		{ href: `/guilds/${guild?.data.id}`, label: guild?.data.name },
+	];
 
-    const isFeaturePage = pathname.startsWith('/features/');
-    const featureName = isFeaturePage ? pathname.split('/').pop() : null;
+	const isFeaturePage = pathname.startsWith('/features/');
+	const featureName = isFeaturePage ? pathname.split('/').pop() : null;
 
-    if (isFeaturePage && featureName) {
-        breadcrumbs.push({ href: pathname, label: featureName });
-    };
+	if (isFeaturePage && featureName) {
+		breadcrumbs.push({ href: pathname, label: featureName });
+	}
 
-    return (
-        <Breadcrumb>
-            <BreadcrumbList>
-                {breadcrumbs.map((crumb, i) => (
-                    <Fragment>
-                        <BreadcrumbItem className="font-sans text-dimWhite text-base">
-                            <BreadcrumbLink href={crumb.href}>
-                                {crumb.label}
-                            </BreadcrumbLink>
-                        </BreadcrumbItem>
-                        {i < breadcrumbs.length - 1 && <BreadcrumbSeparator />}
-                    </Fragment>
-                ))}
-            </BreadcrumbList>
-        </Breadcrumb>
-    )
+	return (
+		<Breadcrumb>
+			<BreadcrumbList>
+				{breadcrumbs.map((crumb, i) => (
+					<Fragment key={crumb.label}>
+						<BreadcrumbItem className="font-sans text-dimWhite text-base">
+							<BreadcrumbLink href={crumb.href}>
+								{crumb.label}
+							</BreadcrumbLink>
+						</BreadcrumbItem>
+						{i < breadcrumbs.length - 1 && <BreadcrumbSeparator />}
+					</Fragment>
+				))}
+			</BreadcrumbList>
+		</Breadcrumb>
+	);
 }
--- apps/frontend/src/components/ui/savebar.tsx
+++ apps/frontend/src/components/ui/savebar.tsx
@@ -1,23 +1,23 @@
-import { UseFormReturn, useFormState } from "react-hook-form";
-import { Button } from "./button";
+import { UseFormReturn, useFormState } from 'react-hook-form';
+import { Button } from './button';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function Savebar({ form }: { form: UseFormReturn<any, any> }) {
-    const state = useFormState(form);
+	const state = useFormState(form);
 
-    return (
-        <div className={`${state.isDirty ? '' : 'hidden'} fixed bottom-2 md:bottom-10 bg-primary rounded-2xl justify-center md:p-[15px] shadow-normal items-center flex-col md:flex-row gap-1 md:gap-2 mt-auto max-w-[95%] w-fit mx-auto z-50`}>
-            <div className="flex flex-row items-center">
-                <h1 className="font-semibold text-md md:text-lg">Hold on, there are unsaved changes! Do you want to save them?</h1>
-                <div className="justify-end ml-auto">
-                    <Button className="bg-secondary mr-2">
+	return (
+		<div className={`${state.isDirty ? '' : 'hidden'} fixed bottom-2 md:bottom-10 bg-primary rounded-2xl justify-center md:p-[15px] shadow-normal items-center flex-col md:flex-row gap-1 md:gap-2 mt-auto max-w-[95%] w-fit mx-auto z-50`}>
+			<div className="flex flex-row items-center">
+				<h1 className="font-semibold text-md md:text-lg">Hold on, there are unsaved changes! Do you want to save them?</h1>
+				<div className="justify-end ml-auto">
+					<Button className="bg-secondary mr-2">
                         Save
-                    </Button>
-                    <Button className="bg-secondary">
+					</Button>
+					<Button className="bg-secondary">
                         Discard
-                    </Button>
-                </div>
-            </div>
-        </div>
-    )
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
 }
