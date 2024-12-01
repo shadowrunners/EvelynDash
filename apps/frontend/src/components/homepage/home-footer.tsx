671b4f1595f3749a71f9b9f2594177ac4a7fee36
scrappie <notscrappie@proton.me>
2024-12-01 13:44:16 +0200

    refactor(frontend): fixed footer styling on mobile
    + styling via eslint

--- apps/frontend/src/components/homepage/home-footer.tsx
+++ apps/frontend/src/components/homepage/home-footer.tsx
@@ -9,64 +9,62 @@ export function Footer() {
 	const legalLinks = [
 		{
 			name: t('footer.legal.privacy'),
-			href: "/privacy",
-			key: "footer_link1"
+			href: '/privacy',
+			key: 'footer_link1',
 		},
 		{
 			name: t('footer.legal.tos'),
-			href: "/tos",
-			key: "footer_link2"
+			href: '/tos',
+			key: 'footer_link2',
 		},
 	];
 
 	const supportLinks = [
 		{
 			name: t('footer.support.server'),
-			href: "placeholder",
-			key: "footer_support_server",
+			href: 'placeholder',
+			key: 'footer_support_server',
 		},
 	];
 
 	const normalLinks = [
 		{
 			name: t('footer.links.github'),
-			href: "https://github.com/spearheadlabs/Evelyn",
-			key: "footer_links_github",
+			href: 'https://github.com/spearheadlabs/Evelyn',
+			key: 'footer_links_github',
 		},
 		{
 			name: t('footer.links.documentation'),
 			href: 'PLACEHOLDER',
-			key: "footer_links_documentation",
+			key: 'footer_links_documentation',
 		},
 		{
 			name: t('footer.links.donations'),
-			href: "https://buymeacoffee.com/scr3ppie",
-			key: "footer_links_donations",
+			href: 'https://buymeacoffee.com/scr3ppie',
+			key: 'footer_links_donations',
 		},
 	];
 
 	return (
-		<div className='flex justify-center items-center sm:py-16 py-6 flex-col'>
-			<div className='flex justify-center items-start md:flex-row flex-col mb-8 w-full'>
-				<div className="flex-1 flex flex-col justify-start mr-10">
-					<h3 className='font-sans text-white w-auto h-auto text-3xl'>Evelyn</h3>
-				</div>
+		<section className='flex flex-col items-center justify-center sm:py-16 py-10 px-6 text-white'>
+			<div className='flex flex-col md:flex-row w-full mb-10 md:gap-10 gap-6'>
+				<h3 className='font-sans text-white text-3xl mb-4 md:mb-0 text-center'>Evelyn</h3>
 
-				<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 text-white w-full flex-row justify-between'>
+				<div className='grid grid-cols-1 md:grid-cols-3 gap-6 w-full text-center md:text-left'>
 					<div className='flex flex-col mx-auto'>
-						<h4 className='font-sans font-medium text-[18px] leading-[27px]'>
+						<h4 className='font-sans font-medium text-lg leading-7'>
 							{t('footer.legal.name')}
 						</h4>
-						<ul className="list-none mt-4 flex flex-col gap-1">
+						<ul className="list-none mt-4 flex flex-col gap-2">
 							{legalLinks.map((item) => (
-								<LocalizedLink href={item.href} key={item.key} className='text-dimWhite transition duration-300 font-sans font-normal text-[16px] leading-[24px] hover:text-white cursor-pointer'>
+								<LocalizedLink href={item.href} key={item.key} className='text-dimWhite transition duration-300 font-sans text-base leading-6 hover:text-white cursor-pointer'>
 									{item.name}
 								</LocalizedLink>
 							))}
 						</ul>
 					</div>
 					<div className='flex flex-col mx-auto'>
-						<h4 className='font-sans font-medium text-[18px] leading-[27px]'>
+						<h4 className='font-sans font-medium text-lg leading-7'>
 							{t('footer.support.name')}
 						</h4>
 						<ul className="list-none mt-4 flex flex-col gap-1">
@@ -76,9 +74,9 @@ export function Footer() {
 								</Link>
 							))}
 						</ul>
-					</div>	
+					</div>
 					<div className='flex flex-col mx-auto'>
-						<h4 className='font-sans font-medium text-[18px] leading-[27px]'>
+						<h4 className='font-sans font-medium text-lg leading-7'>
 							{t('footer.links.name')}
 						</h4>
 						<ul className="list-none mt-4 flex flex-col gap-1">
@@ -97,6 +95,6 @@ export function Footer() {
 					From us to you, built with ❤️ by <a href='https://github.com/notscrappie'>scrappie</a> @ spearhead. © {new Date().getFullYear()}, all rights reserved.
 				</p>
 			</div>
-		</div>
+		</section>
 	);
 };
