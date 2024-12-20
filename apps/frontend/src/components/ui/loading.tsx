613b0f6f86aa59938fe390ce3ae69ba6a7fba459
scrappie <notscrappie@proton.me>
2024-12-20 20:56:44 +0200

    refactor(frontend): replaced react-icons with lucide + lint

--- apps/frontend/src/components/ui/loading.tsx
+++ apps/frontend/src/components/ui/loading.tsx
@@ -1,10 +1,10 @@
-import { LuLoader2 } from "react-icons/lu"
+import { Loader2 } from 'lucide-react';
 
 export function Loader() {
-    return (
-        <div className="flex justify-center items-center flex-col h-screen">
-            <LuLoader2 className="animate-spin h-6 w-6" />
-            <h1 className="mt-2 text-center">Asking the spaceship to bake more cookies...</h1>
-        </div>
-    )
+	return (
+		<div className="flex justify-center items-center flex-col h-screen">
+			<Loader2 className="animate-spin h-6 w-6" />
+			<h1 className="mt-2 text-center">Asking the spaceship to bake more cookies...</h1>
+		</div>
+	);
 }
