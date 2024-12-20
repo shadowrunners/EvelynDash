5a745eb2c65f81ae917d391003162641e30549eb
scrappie <notscrappie@proton.me>
2024-12-20 20:52:12 +0200

    refactor(frontend): save bar now actually works
    shit actually saves now but styling is somewhat broken

--- apps/frontend/src/components/ui/savebar.tsx
+++ apps/frontend/src/components/ui/savebar.tsx
@@ -1,19 +1,33 @@
+'use client';
+
 import { UseFormReturn, useFormState } from 'react-hook-form';
+import { useUpdateFeature } from '@hooks';
 import { Button } from './button';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function Savebar({ form }: { form: UseFormReturn<any, any> }) {
+export function Savebar({ feature, form }: { feature: string, form: UseFormReturn<any, any> }) {
 	const state = useFormState(form);
+	const update = useUpdateFeature();
+
+	const handleSave = form.handleSubmit((data: FormData) => {
+		update.mutate({ feature, data });
+		form.reset();
+	});
+
+	const handleReset = () => form.reset();
 
 	return (
 		<div className={`${state.isDirty ? '' : 'hidden'} fixed bottom-2 md:bottom-10 bg-primary rounded-2xl justify-center md:p-[15px] shadow-normal items-center flex-col md:flex-row gap-1 md:gap-2 mt-auto max-w-[95%] w-fit mx-auto z-50`}>
 			<div className="flex flex-row items-center">
 				<h1 className="font-semibold text-md md:text-lg">Hold on, there are unsaved changes! Do you want to save them?</h1>
 				<div className="justify-end ml-auto">
-					<Button className="bg-secondary mr-2">
+					<Button
+						className="bg-secondary mr-2"
+						onClick={handleSave}
+					>
                         Save
 					</Button>
-					<Button className="bg-secondary">
+					<Button className="bg-secondary" onClick={handleReset}>
                         Discard
 					</Button>
 				</div>
