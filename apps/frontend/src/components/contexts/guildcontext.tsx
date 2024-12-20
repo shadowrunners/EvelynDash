e9fad1b287cecfc8aee5d13975633dfc8ae9621c
scrappie <notscrappie@proton.me>
2024-12-20 16:44:43 +0200

    style(frontend): linting

--- apps/frontend/src/components/contexts/guildcontext.tsx
+++ apps/frontend/src/components/contexts/guildcontext.tsx
@@ -1,8 +1,8 @@
-import { useCurrentGuild } from "@/hooks";
-import { createContext, ReactNode, useContext } from "react";
-import { SPAPIGuild } from "@/types";
-import { Loader } from "../ui/loading";
-import { Error } from "../ui/error";
+import { useCurrentGuild } from '@/hooks';
+import { createContext, ReactNode, useContext } from 'react';
+import { SPAPIGuild } from '@/types';
+import { Loader } from '../ui/loading';
+import { Error } from '../ui/error';
 
 interface Context {
     data: SPAPIGuild;
@@ -11,26 +11,26 @@ interface Context {
 const GuildContext = createContext<Context | null>(null);
 
 export function GuildProvider({ children }: { children: ReactNode }) {
-    const { data, status, error, refetch } = useCurrentGuild();
+	const { data, status, error, refetch } = useCurrentGuild();
 
-    if (status === 'error') return (
-        <div className="bg-black min-h-full text-white">
-            <Error errorMessage={error.message} refetch={refetch} />
-        </div>
-    )
+	if (status === 'error') return (
+		<div className="bg-black min-h-full text-white">
+			<Error errorMessage={error.message} refetch={refetch} />
+		</div>
+	);
 
-    if (status === 'pending') return (
-        <div className="bg-black min-h-full text-white">
-            <Loader />
-        </div>
-    )
+	if (status === 'pending') return (
+		<div className="bg-black min-h-full text-white">
+			<Loader />
+		</div>
+	);
 
-    return (
-        <GuildContext.Provider value={{ data }}>{children}</GuildContext.Provider>
-    )
+	return (
+		<GuildContext.Provider value={{ data }}>{children}</GuildContext.Provider>
+	);
 }
 
 export const useGuild = (): Context | null => {
-    const context = useContext(GuildContext);
-    return context;
-}
+	const context = useContext(GuildContext);
+	return context;
+};
