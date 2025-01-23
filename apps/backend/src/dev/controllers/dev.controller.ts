5368b6346742903de848bb0554c717dbca5030e0
scrappie <notscrappie@proton.me>
2025-01-23 15:01:28 +0200

    feat(backend): applied dev guard + placeholder funcs

--- apps/backend/src/dev/controllers/dev.controller.ts
+++ apps/backend/src/dev/controllers/dev.controller.ts
@@ -9,6 +9,7 @@ import {
 	Req,
 	HttpException,
 	HttpStatus,
+	UseGuards,
 } from '@nestjs/common';
 import { SecureStorage } from '../../utils/secureStorage';
 import { BotService } from '../../bot/services/bot.service';
@@ -16,10 +17,12 @@ import { FastifyRequest } from 'fastify';
 import { config } from 'dotenv';
 
 import { DevService } from '../services/dev.service';
+import { DevGuard } from '../guards/dev.guard';
 
 config();
 
 @Controller('/dev')
+@UseGuards(DevGuard)
 export class DevController {
 	private secureStorage: SecureStorage;
 
@@ -31,30 +34,19 @@ export class DevController {
 	}
 
 	@Get('/blusers')
-	async getBlacklistedUsers(
-		@Req() { headers }: FastifyRequest['raw'],
-	): Promise<unknown> {
-		await this.bot.isOwner(headers.authorization as string);
-
+	async getBlacklistedUsers(): Promise<unknown> {
 		try {
-			const data = await this.dev.getBlacklistedUsers();
-			return data;
+			return await this.dev.getBlacklistedUsers();
 		}
 		catch (_err) {
-			console.log(_err);
 			throw new HttpException('Missing Cyberspace', HttpStatus.NOT_FOUND);
 		}
 	}
 
     @Get('/blguilds')
-    async getBlacklistedGuilds(
-        @Req() { headers }: FastifyRequest['raw'],
-    ): Promise<unknown> {
-        await this.bot.isOwner(headers.authorization as string);
-
+    async getBlacklistedGuilds(): Promise<unknown> {
 		try {
-			const data = await this.dev.getBlacklistedGuilds();
-			return data;
+			return await this.dev.getBlacklistedGuilds();
 		}
 		catch (_err) {
 			console.log(_err);
@@ -63,4 +55,19 @@ export class DevController {
     }
 
 	// TODO: Add patch requests for adding the guild / user to the blacklist.
+
+	@Patch('/blguilds/add')
+	async addGuildToBlacklist() {
+		// this'll get a body with:
+		// {
+		// 		guildId: "123",
+		// 		reason: "testing"
+		// }
+		// blacklist time will be added on the server side
+	}
+
+	@Patch('/blusers/add')
+	async addUserToBlacklist() {
+		// same shit as the above.
+	}
 }
