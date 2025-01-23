9581f6b6faba2fa3469a6e7d0e47f68e4617ec0f
scrappie <notscrappie@proton.me>
2025-01-23 15:03:05 +0200

    refactor(frontend): replaced vortex w/ spotlight on login page

--- apps/frontend/src/app/[locale]/auth/signin/page.tsx
+++ apps/frontend/src/app/[locale]/auth/signin/page.tsx
@@ -10,12 +10,12 @@ import {
 	CardHeader,
 	CardTitle,
 } from '@UI';
-import { Vortex } from '@/components/ui/vortex';
 import { Link } from '@/i18n/routing';
 import { Fragment, useState } from 'react';
 import { Loader2 } from 'lucide-react';
 import { useTranslations } from 'next-intl';
 import { useSearchParams } from 'next/navigation';
+import { Spotlight } from '@components/ui/spotlight';
 
 export default function LoginPage() {
 	const [loading, setLoading] = useState(false);
@@ -25,34 +25,33 @@ export default function LoginPage() {
 	const callbackUrl = useSearchParams().get('callbackUrl') ?? '/';
 
 	return (
-		<div className="w-full min-h-screen bg-black flex justify-center items-center font-sans">
-			<Vortex baseSpeed={0.2} rangeSpeed={0.4}>
-				<Card className="mx-auto max-w-sm bg-secondary/40 backdrop-blur-md">
-					<CardHeader>
-						<CardTitle className="text-xl font-bold">{t('card_title')}</CardTitle>
-						<CardDescription>{t('card_description')} </CardDescription>
-					</CardHeader>
-					<CardContent>
-						<button
-							className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 w-full"
-							onClick={async () => {
-								setLoading(true);
-								await signIn('discord', { callbackUrl });
-							}}
-						>
-							{loading
-								? <Loader2 className="animate-spin" />
-								: <Fragment><FaDiscord className="mr-3" />{t('oauth_button')}</Fragment>
-							}
-						</button>
-					</CardContent>
-					<CardFooter>
-						<p className="text-left text-sm text-muted-foreground">
-							{t('card_footer_part1')}<Link href='/privacy' className="ml-1 hover:text-white">{t('card_footer_link1')}</Link> and<Link href='/tos' className="ml-1 hover:text-white">{t('card_footer_link2')}</Link>.
-						</p>
-					</CardFooter>
-				</Card>
-			</Vortex>
+		<div className="w-full min-h-screen bg-black bg-grid-white/[0.02] flex justify-center items-center font-sans">
+			<Spotlight />
+			<Card className="mx-auto max-w-sm bg-secondary/40 backdrop-blur-md">
+				<CardHeader>
+					<CardTitle className="text-xl font-bold">{t('card_title')}</CardTitle>
+					<CardDescription>{t('card_description')} </CardDescription>
+				</CardHeader>
+				<CardContent>
+					<button
+						className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50 w-full"
+						onClick={async () => {
+							setLoading(true);
+							await signIn('discord', { callbackUrl });
+						}}
+					>
+						{loading
+							? <Loader2 className="animate-spin" />
+							: <Fragment><FaDiscord className="mr-3" />{t('oauth_button')}</Fragment>
+						}
+					</button>
+				</CardContent>
+				<CardFooter>
+					<p className="text-left text-sm text-muted-foreground">
+						{t('card_footer_part1')}<Link href='/privacy' className="ml-1 hover:text-white">{t('card_footer_link1')}</Link> and<Link href='/tos' className="ml-1 hover:text-white">{t('card_footer_link2')}</Link>.
+					</p>
+				</CardFooter>
+			</Card>
 		</div>
 	);
 }
