d7eb6b3c750103cca8c7dfd01f40a6789726fa42
scrappie <notscrappie@proton.me>
2025-01-27 20:29:03 +0200

    feat(frontend): multi select role + channel forms

--- apps/frontend/src/components/forms/channelselectform.tsx
+++ apps/frontend/src/components/forms/channelselectform.tsx
@@ -22,6 +22,7 @@ import {
 	type APIChannel,
 	ChannelType,
 } from 'discord-api-types/v10';
+import { MultiSelect } from '../ui/multi-select';
 
 export function ChannelSelectForm({
 	form,
@@ -67,61 +68,47 @@ export function ChannelSelectForm({
 	);
 }
 
-/**
- * export const MultiChannelSelectForm: ControlledInput<Omit<SelectMenuProps, 'value' | 'onChange'>> = ({
-	control,
-	controller,
-	...props
-}) => {
-	const form = useForm();
-	const guild = useGuildId();
-	const { data, isLoading } = useGuildChannelsQuery(guild);
+export function MultiChannelSelectForm({
+	form,
+	formName,
+	formLabel,
+	formDescription,
+}: FormParams) {
+	const guild = useGuild();
 
-	const options = useMemo(() => {
-		const channels = data?.filter((channel) => channel.type === ChannelTypes.GUILD_TEXT);
-		return channels?.map((channel) => ({ label: channel.name, value: channel.id }));
-	}, [data]);
+	const filteredChannels = guild?.data?.channels.filter((channel) => channel.type === ChannelType.GuildText);
+	const channels = filteredChannels?.map((channel) => ({ label: channel.name, value: channel.id }));
 
 	return (
-		<div className='grid gap-3'>
-			<div className="flex flex-col width-[100%] relative border-r-3xl p-5 shadow bg-secondary rounded-3xl">
-				<label className="block text-start mr-3 transition-all duration-300 opacity-100 text-base font-medium mb-0">
-					<h2 className="text-2xl font-semibold">{control.label}</h2>
-					<p className="text-gray-500 mb-3">{control.description}</p>
-				</label>
-				<Spacer />
-				<Form {...form}>
-					<FormField
-						control={controller.control}
-						name={controller.name}
-						render={({ field }) => {
-							const selected = useMemo(() => {
-								const selectedValues = new Set(field.value.map((channel: { value: string; }) => typeof channel === 'string' ? channel : channel?.value));
-								return options?.filter((channel) => selectedValues.has(channel?.value));
-							}, [options, field.value]);
-
-							return (
-								<FormItem>
-									<FormControl>
-										<MultipleSelector
-											{...field}
-											{...props}
-											label='Text Channels'
-											placeholder='Select a channel.'
-											options={options}
-											disabled={isLoading}
-											value={selected}
-											className='bg-secondary text-white'
-										/>
-									</FormControl>
-									<FormMessage />
-								</FormItem>
-							);
-						}}
-					/>
-				</Form>
-			</div>
-		</div>
+		<Form {...form}>
+			<form className='bg-muted/40 width-[50%] relative border-r-3xl space-y-6 p-4 shadow rounded-xl'>
+				<FormField
+					control={form.control}
+					name={formName}
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>
+								{formLabel}
+							</FormLabel>
+							<FormControl>
+								<MultiSelect
+									{...field}
+									placeholder='Select a channel.'
+									// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+									// @ts-ignore
+									options={channels}
+									className='bg-secondary text-white'
+									onValueChange={field.onChange}
+								/>
+							</FormControl>
+							<FormDescription>
+								{formDescription}
+							</FormDescription>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+			</form>
+		</Form>
 	);
 };
- */
--- apps/frontend/src/components/forms/roleselectform.tsx
+++ apps/frontend/src/components/forms/roleselectform.tsx
@@ -16,6 +16,7 @@ import {
 	FormMessage,
 } from '@UI';
 import { useGuild } from '../contexts/guildcontext';
+import { MultiSelect } from '../ui/multi-select';
 
 export function RoleSelectForm({
 	form,
@@ -60,68 +61,44 @@ export function RoleSelectForm({
 	);
 };
 
-/**
- * export const MultiRoleSelect = ({
-	value, onChange, ...rest
-}: {
-	value: SelectMenuOptionArray | string[];
-	onChange: (role: SelectMenuOptionArray) => void;
-}) => {
-	const guild = useGuildId();
-	const { data, isLoading } = useGuildRolesQuery(guild);
-
-	const options = useMemo(() => {
-		return data?.map((role) => ({ label: role.name, value: role.id }));
-	}, [data]);
-
-	const selected = useMemo(() => {
-		const selectedValues = new Set(value.map((role) => typeof role === 'string' ? role : role?.value));
-		return options?.filter((role) => selectedValues.has(role?.value));
-	}, [options, value]);
+export function MultiRoleSelectForm({
+	form,
+	formName,
+	formLabel,
+	formDescription,
+}: FormParams) {
+	const guild = useGuild();
+	const options = guild?.data.roles.map((role) => ({ label: role.name, value: role.id }));
 
 	return (
-		<SelectMenu
-			isLoading={isLoading}
-			isDisabled={isLoading}
-			value={selected}
-			placeholder='Select a role.'
-			options={options}
-			onChange={(e) => onChange((e as SelectMenuOptionArray))}
-			{...rest}
-		/>
+		<Form {...form}>
+			<form className='bg-muted/40 width-[50%] relative border-r-3xl space-y-6 p-4 shadow rounded-xl'>
+				<FormField
+					control={form.control}
+					name={formName}
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>
+								{formLabel}
+							</FormLabel>
+							<FormControl>
+								<MultiSelect
+									{...field}
+									placeholder='Select some roles.'
+									// @ts-ignore
+									options={options}
+									className='bg-secondary text-white'
+									onValueChange={field.onChange}
+								/>
+							</FormControl>
+							<FormDescription>
+								{formDescription}
+							</FormDescription>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+			</form>
+		</Form>
 	);
 };
-
-// this'll carry on like this, I genuinely do not want to write this implementation ever again.
-// ported this shit over from role select, if it breaks, it breaks
-export const MultiRoleSelectForm: ControlledInput<Omit<SelectMenuProps, 'value' | 'onChange'>> = ({
-	control,
-	controller,
-	...props
-}) => {
-	const form = useForm();
-
-	return (
-		<div className='grid gap-3'>
-			<div className="flex flex-col width-[100%] relative border-r-3xl p-5 shadow bg-secondary rounded-3xl">
-				<label className="block text-start mr-3 transition-all duration-300 opacity-100 text-base font-medium mb-0">
-					<h2 className="text-2xl font-semibold">{control.label}</h2>
-					<p className="text-gray-500 mb-3">{control.description}</p>
-				</label>
-				<Spacer />
-				<Form {...form}>
-					<form>
-						<FormField
-							control={controller.control}
-							name={controller.name}
-							render={({ field }) => (
-								<MultiRoleSelect {...field} {...props} />
-							)}
-						/>
-					</form>
-				</Form>
-			</div>
-		</div>
-	);
-};
- */
